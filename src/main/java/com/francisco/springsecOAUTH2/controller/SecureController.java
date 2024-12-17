d1d92be03fb993cfc33190c2bdd000d4de56720c
Francisco Sousa <franciscoeds1987@gmail.com>
2024-12-17 08:00:21 -0300

    Demo of OAUTH2 using social logins - Part 1

---
+++ src/main/java/com/francisco/springsecOAUTH2/controller/SecureController.java
@@ -0,0 +1,14 @@
+package com.francisco.springsecOAUTH2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecureController {
+
+    @GetMapping("/secure")
+    public String securePage() {
+        return "secure.html";
+    }
+
+}
